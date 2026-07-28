c1d031e6c52783b43f9eb584240c9b8703c7c252
disfracesmaysa <disfracesmaysa@gmail.com>
2026-07-27 23:16:32 -0500

    Update cliente.js

--- js/cliente.js
+++ js/cliente.js
@@ -38,8 +38,8 @@ const CLIENTE = {
   // El header conserva siempre su tamaño; si excedes el espacio disponible,
   // la imagen se limita automáticamente dentro de él.
   // Pon 0 para que esa medida se ajuste automáticamente.
-  LOGO_NOMBRE_ANCHO_PC: 300,
-  LOGO_NOMBRE_ALTO_PC: 360,
+  LOGO_NOMBRE_ANCHO_PC: 220,
+  LOGO_NOMBRE_ALTO_PC: 180,
   LOGO_NOMBRE_ANCHO_CELULAR: 190,
   LOGO_NOMBRE_ALTO_CELULAR: 100,
 
